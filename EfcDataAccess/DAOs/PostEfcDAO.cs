d14153a95f310e549115d2ecd982939ec80d8522
Lovrevar <lovrevar@hotmail.com>
2022-11-22 14:12:48 +0100

    e

---
+++ EfcDataAccess/DAOs/PostEfcDAO.cs
@@ -0,0 +1,63 @@
+﻿using Application.DaoInterfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Shared.Dtos;
+using Shared.Model;
+
+namespace EfcDataAccess.DAOs;
+
+public class PostEfcDAO : IPostDao
+{
+    private readonly PostContext context;
+
+    public PostEfcDAO(PostContext context)
+    {
+        this.context = context;
+    }
+    public async Task<Post> CreateAsync(Post post)
+    {
+        EntityEntry<Post> added = await context.Posts.AddAsync(post);
+        await context.SaveChangesAsync();
+        return added.Entity;
+    }
+    public async Task<IEnumerable<Post>> GetAsync(SearchPostParametersDto searchParameters)
+    {
+        IQueryable<Post> result = context.Posts.Include(post => post.user).AsQueryable();
+
+        if (!string.IsNullOrEmpty(searchParameters.Username))
+        {
+            result = result.Where(post =>
+                post.user.Username.ToLower().Equals(searchParameters.Username.ToLower()));
+        }
+        
+        
+        if (!string.IsNullOrEmpty(searchParameters.TitleContains))
+        {
+            result = result.Where(t =>
+                t.title.ToLower().Contains(searchParameters.TitleContains.ToLower()));
+        }
+
+        List<Post> david = await result.ToListAsync();
+        return david;
+    }
+    public Task<Post> GetByIdAsync(int id)
+    {
+        Post? existing = context.Posts.FirstOrDefault(p => p.postId == id);
+        return Task.FromResult(existing);
+    }
+    public Task UpdateAsync(Post post)
+    {
+        throw new NotImplementedException();
+    }
+    public async Task DeleteAsync(int id)
+    {
+        Post? existing = await GetByIdAsync(id);
+        if (existing == null)
+        {
+            throw new Exception($"Post with id {id} not found");
+        }
+
+        context.Posts.Remove(existing);
+        await context.SaveChangesAsync();
+    }
+}
---
+++ EfcDataAccess/Migrations/20221122123505_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfcDataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Role = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    postId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    title = table.Column<string>(type: "TEXT", nullable: false),
+                    content = table.Column<string>(type: "TEXT", nullable: false),
+                    Username = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.postId);
+                    table.ForeignKey(
+                        name: "FK_Posts_Users_Username",
+                        column: x => x.Username,
+                        principalTable: "Users",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_Username",
+                table: "Posts",
+                column: "Username");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
